98e4c7616acdbf9cbf5cd30b06c95a924b1ede3e
salgu1998 <salgu1998@gmail.com>
2022-11-03 19:07:04 +0900

    백준 - 1931: 회의실 배정 - greedy

---
+++ src/code/Baekjoon1.java
@@ -0,0 +1,17 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 백준 - 123: example - example
+ * https://www.acmicpc.net/problem/123
+ */
+public class Baekjoon1 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
+
